cce884ff0f52c33a9734f68e27ba6723e22ac128
GenDecoder <mariomedranomaldonado@gmail.com>
2017-04-28 18:25:39 -0400

    Improvement

--- index.js
+++ index.js
@@ -2,34 +2,17 @@ var _ = require("underscore");
 var mapper = require("./lib/util/mapper");
 var dp = require("./lib/util/design-patterns");
 var pathManager = require("./lib/util/path-manager");
-var base = {        
+var bases = {
     "Nx.Model": require("./lib/base/model"),
     "Nx.Resource": require("./lib/base/resource"),
     "Nx.Middleware": require("./lib/base/middleware"),
     "Nx.Application": require("./lib/base/application")
 };
-var bases = [
-    "Nx.Model",
-    "Nx.Resource",
-    "Nx.Middleware",
-    "Nx.Application"    
-];
-// Model: "",
-// Resource: "", // posiblemente la instancia no se llame aca y tenga que recibir el router como parametro
-// Middleware: "", // posiblemente la instancia no se llame aca y tenga que recibir el router como parametro
-// Application: "", // basicamente es un singleton - cuyo contenido esta expuesto (quiza no debiese existir)
-
-if (!global.Nx) {
-    // global.Nx = {      
-    //     define: mapper.setExposedSource, // CAN BE IMPROVED
-    // };
-    global.Nx = {}; // a singleton
-    /**
-     * NX will have the same methods that underscore has.
-     */
+if (!global.Nx) {    
+    global.Nx = {}; // a singleton?
+    // NX will have the same methods that underscore has.
     _.extend(Nx, _);
 }
-
 /**
  * Class constructor.
  * @param {Object} config 
@@ -39,52 +22,42 @@ function Nox(config) {
     var router = config.router;
     var name = config.name || "APP";
     var folder = process.cwd() + "/" + config.folder + "/";        
-    var group = { // set to {} at the end
+    var group = {
         mixin: [],
-        singleton: [],        
+        singleton: [],
+
         model: [],
         resource: [],
         middleware: [],
         application: []
     };
-    var sourceMap = {}; // set to {} at the end
+    var sourceMap = {};
     var pathMap = pathManager.getPathMap(name, folder);
     try {
-        /**
-         * Validate Application Name (Should be unique because of the global variable used).
-         */
+        // Validate Application Name (Should be unique because of the global variable used).
         if (!global[name])
             global[name] = {};
         else
             throw new Error("Application with name: " + name + " is already in use.");
-        /**
-         * Add "Define" method to "Nx"
-         */
+        // Add "Define" method to "Nx".
         Nx.define = function(nxPath, scope) {
             if (Nx.contains(pathMap.js.nxPathList, nxPath)) 
-                 mapper.addExposedMap(nxPath, scope);
+                 mapper.addExposedSource(nxPath, scope);
             else
                  throw new Error(nxPath + " not found, review the file structure or look for typo errors in the Nx.define, there must exists consistency");
         };        
-        /**
-         * Trigger the "Nx.define" method for all Sources.
-         */
+        // Trigger the "Nx.define" method for all Sources.
         _.each(pathMap.js.absolutePathList, function iterator(absolutePath) {
             require(absolutePath);
         });
-        /**
-         * 
-         */
+        //
         _.each(pathMap.js.nxPathList, function iterator(nxPath) {
             var object;            
             var parent;
-            var road = [];
             var groupName;
-
-            /**
-             * Create roads
-             */
-            while(nxPath && !Nx.contains(bases, nxPath)) {
+            var road = [];
+            // Create roads.
+            while(nxPath && !bases.hasOwnProperty(nxPath)) {
                 road.push(nxPath);
                 object = mapper.getExposedSource(nxPath);
                 if (object)
@@ -92,10 +65,9 @@ function Nox(config) {
                 else    
                     throw new Error(nxPath + " is not defined");
             }
-
-            // GROUP NAME
-            if (Nx.contains(bases, nxPath)) {
-                parent = base[nxPath];
+            // Assign a group  to each sources.
+            if (bases.hasOwnProperty(nxPath)) {
+                parent = bases[nxPath];
                 groupName = nxPath.split('.')[1].toLowerCase();
             } else
                 groupName = "mixin"; // ANOTHER proccess NEEDED
@@ -116,21 +88,26 @@ function Nox(config) {
                 object = mapper.getExposedSource(nxPath);
                 if (!sourceMap[nxPath]) {
                     group[groupName].push(nxPath);
-                    sourceMap[nxPath] = dp.createClass(object, parent, router)                                       
+                    sourceMap[nxPath] = dp.createClass(object, parent, router);
                 }
             });
         });
 
         _.each(sourceMap, function iterator(sourceClass, nxPath) {
             mapper.setExposedSource(nxPath, sourceClass);
+            var instance = new sourceClass();
+            instance.init && instance.init();            
         });
 
-           // Init per group
+
+
+        // Init per group
             // First:   Application (Singleton) // validate only one
             // Second:  Models
             // Thrid:   Middlewares
             // Fourth:  Resources
 
+        // Clean variables
         group = {};
         sourceMap = {};
         delete Nx.define;
